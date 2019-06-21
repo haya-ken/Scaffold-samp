04259cfb5ea360fdcfec3837a31b906b63c2d658
hayashi kenji <khayashi716@yahoo.co.jp>
2019-06-21 20:53:10 +0900

    コントローラーにCRUDアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'diary/title:string'
+  get 'diary/body:text'
+  resources :diaries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get'/users', to:'users#index'
   get'/users/new'
